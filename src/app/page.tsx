b8d4246ebf66441f13e6505ddb11d47cc9f3de2a
Javeed Mahammad <mahammadjaveed170@gmail.com>
2025-09-02 04:23:49 +0000

    for medium screens, it is working. for larger screens like in pc, it is

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,11 +9,13 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-grow container mx-auto px-6 sm:px-10 lg:px-16">
-        <Hero />
-        <About />
-        <Work />
-        <Contact />
+      <main className="flex-grow">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <Hero />
+          <About />
+          <Work />
+          <Contact />
+        </div>
       </main>
       <Footer />
     </div>
